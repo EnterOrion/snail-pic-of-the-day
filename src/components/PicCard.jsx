d730ca5255fc3d69e964d7568313fcef8ba15bba
EnterOrion <orionwesson@gmail.com>
2023-04-18 01:06:54 +0530

    Polish pic page

--- src/components/PicCard.jsx
+++ src/components/PicCard.jsx
@@ -4,9 +4,20 @@ const PicCard = (props) => {
   return (
     <div className="card-container">
       <img className="card-pic" src={props.image} alt={props.alt} />
-      <span class="card-footer">
-        <span>{props.date}</span>
-      </span>
+      <div class="card-footer">
+        <p className="card-date">{props.date}</p>
+        <p className="card-detail">
+          <span>{props.date} </span> <br />
+          <ul>
+            <li>
+              <strong>Category:</strong> <a href="">2D Snail</a>
+            </li>
+            <li>
+              <strong>Description:</strong> A snail drawing on a museum display
+            </li>
+          </ul>
+        </p>
+      </div>
     </div>
   );
 };
